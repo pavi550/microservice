cdd24da6bd20e44be2b970eaa0e16863781fa2f6
pavi550 <pavi550@gmail.com>
2026-04-24 00:21:52 +0530

    Provier created

---
+++ terraform/version.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Owner = "kumaprap"
+    }
+  }
+}
