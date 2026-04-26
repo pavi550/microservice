c708080254b10966f717ec57dfece29ca4f42467
pavi550 <pavi550@gmail.com>
2026-04-26 11:09:01 +0530

    helm fix

--- terraform/monitoring/version.tf
+++ terraform/monitoring/version.tf
@@ -33,7 +33,19 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks_cluster.token
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        local.cluster_name,
+        "--region",
+        var.region,
+      ]
+    }
   }
 }
 
@@ -43,9 +55,5 @@ variable "region" {
 }
 
 data "aws_eks_cluster" "eks_cluster" {
-  name = local.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks_cluster" {
   name = local.cluster_name
 }
