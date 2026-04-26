6607408c0c364646c3bec6f0255e6a41e2b30593
pavi550 <pavi550@gmail.com>
2026-04-26 10:06:29 +0530

    compute modify

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -1,6 +1,6 @@
 locals {
   cluster_name     = "my-eks-cluster-v2"
-  namespace        = "default"
+  namespace        = "monitoring"
   chart_name       = "kube-prometheus-stack"
   chart_repository = "https://prometheus-community.github.io/helm-charts"
   chart_version    = "57.1.1"
@@ -13,10 +13,44 @@ resource "helm_release" "monitoring" {
   chart      = local.chart_name
   version    = local.chart_version
   namespace  = local.namespace
+  create_namespace = true
+  timeout    = 900
+  atomic     = true
 
   set {
     name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
     value = "false"
+  }
+
+  # Reduce default requests so the chart schedules on smaller EKS node groups.
+  set {
+    name  = "grafana.resources.requests.cpu"
+    value = "50m"
+  }
+
+  set {
+    name  = "grafana.resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.resources.requests.cpu"
+    value = "100m"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.resources.requests.memory"
+    value = "512Mi"
+  }
+
+  set {
+    name  = "alertmanager.alertmanagerSpec.resources.requests.cpu"
+    value = "50m"
+  }
+
+  set {
+    name  = "alertmanager.alertmanagerSpec.resources.requests.memory"
+    value = "128Mi"
   }
 }
 
